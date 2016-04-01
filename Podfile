2698f8cc909c6193f83f20416d06c72c53bdccfc
Baldoph Pourprix <baldoph.pourprix@worldline.com>
2016-04-01 17:22:40 +0200

    Created WeatherKit target, added RxSwift, Argo, Swinject... via Cocoapods

--- Podfile
+++ Podfile
@@ -1,3 +1,22 @@
+platform :ios, '8.0'
 use_frameworks!
 
-pod 'Alamofire', '~> 3.0'
+target 'weather' do
+
+pod 'RxSwift'
+pod 'RxCocoa'
+pod 'Alamofire'
+pod 'Argo'
+pod 'Curry'
+pod 'Swinject'
+
+end
+
+target 'WeatherKit' do
+
+    pod 'RxSwift'
+    pod 'Alamofire'
+    pod 'Argo'
+    pod 'Curry'
+
+end
---
+++ WeatherKit/WeatherKit.h
@@ -0,0 +1,19 @@
+//
+//  WeatherKit.h
+//  WeatherKit
+//
+//  Created by Baldoph Pourprix on 01/04/16.
+//  Copyright © 2016 Baldoph Pourprix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherKit.
+FOUNDATION_EXPORT double WeatherKitVersionNumber;
+
+//! Project version string for WeatherKit.
+FOUNDATION_EXPORT const unsigned char WeatherKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherKit/PublicHeader.h>
+
+
